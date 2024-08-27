c26c8706476686e12e280b13719f5047dc0c0791
AllanAlmeidaDev8 <allanalmeidadev8@gmail.com>
2024-08-27 17:52:06 -0300

    Update Tiragosto.jsx

--- src/pages/Tiragosto.jsx
+++ src/pages/Tiragosto.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Tiragosto.css';
+import './Button.css';
 
 const Tiragosto = () => {
     const navigate = useNavigate();
